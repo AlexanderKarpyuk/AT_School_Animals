623412567ed06e1df106a7132089ff2938ffbae7
Alexander <karjuk.sasha@gmail.com>
2020-06-30 12:47:19 +0400

    Добавил еще еды и комментарии в класс Zoo.

--- src/java/Zoo/Zoo.java
+++ src/java/Zoo/Zoo.java
@@ -1,15 +1,21 @@
 package Zoo;
 
 import Animals.*;
+import Food.*;
 
 public class Zoo {
     public static void main(String[] args) {
+        //Создаём хранилище с едой.
+        Food[] foods = new Food[] {new Beef(), new Carrot(), new Grass(), new Bull()};
+
+        //Создаём вольеры и указываем их размеры.
         AviaryCarnivorous aviaryCarnivorous1 = new AviaryCarnivorous(5);
         AviaryCarnivorous aviaryCarnivorous2 = new AviaryCarnivorous(4);
 
         AviaryHerbivore aviaryHerbivore1 = new AviaryHerbivore(6);
         AviaryHerbivore aviaryHerbivore2 = new AviaryHerbivore(3);
-        
+
+        //Загоняем животных в вольеры.
         for (int i = 0; i < aviaryCarnivorous1.getSize(); i++) {
             aviaryCarnivorous1.addAnimal(new Tiger());
         }
