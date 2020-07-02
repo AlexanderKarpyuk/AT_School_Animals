5732364690606ca8b0835c3c1e90749a350bc9a7
Alexander <karjuk.sasha@gmail.com>
2020-07-02 13:27:27 +0400

    Добавил джавадоки

--- src/java/Exceptions/WrongFoodException.java
+++ src/java/Exceptions/WrongFoodException.java
@@ -1,5 +1,9 @@
 package Exceptions;
 
+/**
+ * Исключение выбрасывается в случае, если попробовать накормить животное не верным типом еды.
+ * Из перечисления Food.
+ */
 public class WrongFoodException extends Exception {
     private String message;
 
