808a3639bcd3ca936af775f1afe3591637b376c1
Alexander <karjuk.sasha@gmail.com>
2020-07-07 11:38:02 +0400

    переместил создание вольеров и животных в отдельный метод

--- src/main/java/ru/lanit/zoo/Zoo.java
+++ src/main/java/ru/lanit/zoo/Zoo.java
@@ -17,45 +17,15 @@ public class Zoo {
 
     public static void main(String[] args) {
         //Создаём хранилище с едой.
-        loggerConsole.info("Создаётся хранилище с едой");
-        loggerFile.info("Создаётся хранилище с едой");
+        loggerConsole.info("Создаём хранилище с едой");
+        loggerFile.info("Создаём хранилище с едой");
         Food[] foods = Food.values();
 
-        //Создаём вольеры и указываем их размеры.
-        loggerConsole.info("Создаём вольеры");
-        loggerFile.info("Создаём вольеры");
-        AviaryCarnivorous aviaryCarnivorous1 = new AviaryCarnivorous(5);
-        AviaryCarnivorous aviaryCarnivorous2 = new AviaryCarnivorous(4);
+        //Создаём вольеры.
+        loggerConsole.info("Создаём вольеры и загоняем туда животных");
+        loggerFile.info("Создаём вольеры и загоняем туда животных");
+        List<Aviary> aviaries = getListAviary();
 
-        AviaryHerbivore aviaryHerbivore1 = new AviaryHerbivore(6);
-        AviaryHerbivore aviaryHerbivore2 = new AviaryHerbivore(3);
-
-        //Загоняем животных в вольеры.
-        loggerConsole.info("Загоняем животных в вольеры");
-        loggerFile.info("Загоняем животных в вольеры");
-        for (int i = 0; i < aviaryCarnivorous1.getSize(); i++) {
-            aviaryCarnivorous1.addAnimal(new Tiger());
-        }
-        for (int i = 0; i < 2; i++) {
-            aviaryCarnivorous2.addAnimal(new Lion());
-        }
-
-        for (int i = 0; i < 2; i++) {
-            aviaryHerbivore1.addAnimal(new Zebra());
-        }
-
-        for (int i = 0; i < aviaryHerbivore2.getSize(); i++) {
-            aviaryHerbivore2.addAnimal(new Giraffe());
-        }
-
-        //Заносим вольеры в список, чтобы было проще с ними работать
-        loggerConsole.info("Заносим вольеры в список");
-        loggerFile.info("Заносим вольеры в список");
-        List<Aviary> aviaries = new ArrayList<>();
-        aviaries.add(aviaryHerbivore1);
-        aviaries.add(aviaryHerbivore2);
-        aviaries.add(aviaryCarnivorous1);
-        aviaries.add(aviaryCarnivorous2);
 
         //Стажёру поручено покормить животных,но он может ошибиться и дать им не то что нужно.
         for (Aviary aviary : aviaries) {
@@ -66,10 +36,12 @@ public class Zoo {
                 System.out.println("Работник зоопарка для " + animal + " наложил еды: "
                         + food);
                 try {
-                    loggerConsole.info("Стажёр пытается покормить животного");
+                    loggerConsole.info("Стажёр пытается покормить животное");
+                    loggerFile.info("Стажёр пытается покормить животное");
                     animal.eat(food);
                 } catch (WrongFoodException e) {
                     loggerConsole.error("Не получилось покормить животное");
+                    loggerFile.error("Не получилось покормить животное");
                     System.out.println(e.getMessage());
                 }
             }
@@ -85,6 +57,32 @@ public class Zoo {
         duck.say();
         System.out.println("Сотрудник пытается поймать утку, но безуспешно");
         duck.say();
+    }
+
+
+    private static List<Aviary> getListAviary() {
+        List<Aviary> list = new ArrayList<>();
+        list.add(new AviaryCarnivorous(5));
+        list.add(new AviaryCarnivorous(4));
+        list.add(new AviaryHerbivore(6));
+        list.add(new AviaryHerbivore(3));
+
+        for (int i = 0; i < list.get(0).getSize(); i++) {
+            list.get(0).addAnimal(new Tiger());
+        }
+        for (int i = 0; i < 2; i++) {
+            list.get(1).addAnimal(new Lion());
+        }
+
+        for (int i = 0; i < 2; i++) {
+            list.get(2).addAnimal(new Zebra());
+        }
+
+        for (int i = 0; i <list.get(3).getSize(); i++) {
+            list.get(3).addAnimal(new Giraffe());
+        }
+
+        return list;
     }
 
 }
